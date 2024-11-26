064a093a3777aa0ef44099fd4ff9e11d230a97ee
Michael <michael.njoroge3@student.moringaschool.com>
2024-11-26 22:55:31 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,21 @@
+function divide(){
+    let a= 2000
+    let b =100
+    return a/b;
+}
+
+
+
+
+let x =2;
+ let square = x => x * x;
+ x*x === 4;
+ 
+
+
+
+ let a =3;
+ let b =4;
+ let add =(a,b) =>a+b;
+ a+b === 7;
+ 
